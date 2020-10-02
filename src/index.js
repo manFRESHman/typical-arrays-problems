b6cf13503c82e1e5f9371e8328ab933da195cb0e
Igor Tolobaev <igortolobaev@gmail.com>
2020-10-03 02:50:50 +0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array || !array.length){
+    return 0;
+  }
+  let mn = array[0];
+  for(el of array){
+    if(el < mn){
+      mn = el;
+    }
+  }
+  return mn
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || !array.length){
+    return 0;
+  }
+  let mx = array[0];
+  for(el of array){
+    if(el > mx){
+      mx = el;
+    }
+  }
+  return mx
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || !array.length){
+    return 0;
+  }
+  let sm = 0;
+  for(el of array){
+    sm += el;
+  }
+  return sm / array.length;
 }
